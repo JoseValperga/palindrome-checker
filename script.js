d09f2f3a2e4f1c316fb8c89e8996fdeb2e3b0dc3
Jose Valperga <jose.valperga@gmail.com>
2025-03-14 19:16:39 -0300

    Refactorizo script.js para que tome el enter y el click

--- script.js
+++ script.js
@@ -5,6 +5,35 @@ const isPalindrome = (input) => {
   return cleanedInput === reverseInput;
 };
 
+const handleCheck = () => {
+  const userInput = document.getElementById("text-input").value.trim();
+  
+  if (!userInput) {
+    alert("Please input a value");
+    return;
+  }
+  
+  isPalindrome(userInput)
+    ? (document.getElementById("result").textContent = `${userInput} is a palindrome`)
+    : (document.getElementById("result").textContent = `${userInput} is not a palindrome`);
+};
+
+document.getElementById("check-btn").addEventListener("click", handleCheck);
+
+document.getElementById("text-input").addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    handleCheck();
+  }
+});
+
+
+/*
+const isPalindrome = (input) => {
+  const cleanedInput = input.toLowerCase().replace(/[^a-z0-9]/g, "");
+  const reverseInput = cleanedInput.split("").reverse().join("");
+  return cleanedInput === reverseInput;
+};
+
 document.getElementById("check-btn").addEventListener("click", () => {
   const userInput = document.getElementById("text-input").value.trim();
   if (!userInput) {
@@ -19,4 +48,5 @@ document.getElementById("check-btn").addEventListener("click", () => {
         "result"
       ).textContent = `${userInput} is not a palindrome`);
 });
+*/
 
