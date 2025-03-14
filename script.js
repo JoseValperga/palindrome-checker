8d3d6bdad0662f0bcc63466f02be75508655359c
Jose Valperga <jose.valperga@gmail.com>
2025-03-14 18:58:36 -0300

    Refactorización inicial de  script.js -> creo la funcion isPalindrome que chequea

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
+
+const isPalindrome = (input) => {
+  const cleanedInput = input.toLowerCase().replace(/[^a-z0-9]/g, "");
+  const reverseInput = cleanedInput.split("").reverse().join("");
+  return cleanedInput === reverseInput;
+};
+
 document.getElementById("check-btn").addEventListener("click", () => {
-    const userInput = document.getElementById("text-input").value.trim();
-  
-    if (!userInput) {
-      alert("Please input a value");
-      return;
-    }
-  
-    const cleanedInput = userInput.toLowerCase().replace(/[^a-z0-9]/g, "");
-    const reverseInput = cleanedInput.split("").reverse().join("");
-  
-    if (cleanedInput === reverseInput) {
-      document.getElementById("result").textContent = `${userInput} is a palindrome`;
-    } else {
-      document.getElementById("result").textContent = `${userInput} is not a palindrome`;
-    }
-  });
-  
+  const userInput = document.getElementById("text-input").value.trim();
+  if (!userInput) {
+    alert("Please input a value");
+    return;
+  }
+  isPalindrome(userInput)
+    ? (document.getElementById(
+        "result"
+      ).textContent = `${userInput} is a palindrome`)
+    : (document.getElementById(
+        "result"
+      ).textContent = `${userInput} is not a palindrome`);
+});
+
